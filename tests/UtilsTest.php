bcdffbb3bede151125a1fd05c01afdff3abdaee7
Neizzzy <azat.azatch@yandex.ru>
2025-03-20 20:08:42 +0300

    Add test with fixtures

--- tests/UtilsTest.php
+++ tests/UtilsTest.php
@@ -8,9 +8,20 @@ use function Hexlet\Phpunit\Utils\reverseString;
 
 class UtilsTest extends TestCase
 {
+    public function getFixtureFullPath($fileName): string
+    {
+        $parts = [__DIR__, 'fixtures', $fileName];
+        return realpath(implode('/', $parts));
+    }
+
     public function testReverse(): void
     {
         $this->assertEquals('', reverseString(''));
         $this->assertEquals('olleh', reverseString('hello'));
+
+        $pathToHtmlBefore = $this->getFixtureFullPath('before.html');
+        $pathToHtmlAfter = $this->getFixtureFullPath('after.html');
+        $html = reverseString(file_get_contents($pathToHtmlBefore));
+        $this->assertStringEqualsFile($pathToHtmlAfter, $html);
     }
 }
